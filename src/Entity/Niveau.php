1f4fc5a5127f5449a15fc44c2666f6127532f85e
Axel Stankiewicz <axelxbox126@gmail.com>
2022-02-22 19:35:59 +0100

    Phase 2 : ORM (lien entre les deux tables, type association ManyToOne).

---
+++ src/Entity/Niveau.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NiveauRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NiveauRepository::class)
+ */
+class Niveau
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $niveau;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNiveau(): ?string
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(string $niveau): self
+    {
+        $this->niveau = $niveau;
+
+        return $this;
+    }
+}
